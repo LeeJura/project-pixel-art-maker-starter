7062c23065a292fba42021b0f0f887684774234b
LeeJura <lastarda@gmail.com>
2017-10-10 13:47:24 -0500

    Add classes to columns and rows

--- designs.js
+++ designs.js
@@ -1,4 +1,5 @@
 // Select color input
+colorInput = $("#colorPicker");
 
 // Select size input
 
@@ -9,40 +10,41 @@ const gridWidth = $("#input_width");
 
 function createBody() {
     $("<tbody></tbody>").appendTo("#pixel_canvas");
-}
+};
 
 //Create grid row
 
 function createRow() {
     for (let i = 1; i <= gridHeight.val(); i++) {
-        $("<tr></tr>").appendTo("tbody");
+        $("<tr class=row></tr>").appendTo("tbody");
     };
-}
+};
 
 //Create grid column
 
 function createColumn() {
     for (let i = 1; i <= gridWidth.val(); i++) {
-        $("<td></td>").appendTo("tr");
+        $("<td class=column></td>").appendTo("tr");
     };
-}
+};
 
-// When size is submitted by the user, call makeGrid()
+// Create full grid
 
 function makeGrid() {
     removeBody();
     createBody();
     createRow();
     createColumn();
-}
-
-$("#sizePickerButton").click(function() {
-    makeGrid();
-    return false;
-});
+};
 
 // Remove table body
 
 function removeBody() {
     $("tbody").remove();
-}
+};
+
+// When size is submitted by the user, call makeGrid()
+$("#sizePickerButton").click(function() {
+    makeGrid();
+    return false;
+})
