72f3c2112a6aed3a08038def0fc7144db8cc71d2
LeeJura <lastarda@gmail.com>
2017-10-10 10:24:23 -0500

    Let submit button create 1 grid box

--- designs.js
+++ designs.js
@@ -1,5 +1,18 @@
 // Select color input
+
 // Select size input
+const box = $("<tr><td></td></tr>");
+
+function createBox() {
+    $(box).appendTo("#pixel_canvas");
+};
+
+$("#sizePickerButton").click(function() {
+    createBox();
+    return false;
+    console.log("event clicked");
+});
+
 
 // When size is submitted by the user, call makeGrid()
 
